b6bff623d1be09358a6d5936ba9f130890a66401
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2022-01-07 01:04:15 -0500

    I have been using parameter input to guard against SQL injection (discussed by Francis in class) from the start, just confirming the walkthrough

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,13 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
   JOIN students ON assistance_requests.student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%' || $1 || '%'
+  WHERE cohorts.name LIKE '%' || $1 || '%' 
   GROUP BY cohorts.name, teachers.name
   ORDER BY teachers.name;
 `, [process.argv[2]])
